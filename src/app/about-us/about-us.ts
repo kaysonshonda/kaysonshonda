9d451e72a2b4711433b647d6a2c835ae9b83c52e
DESKTOP-PLV51FF\DELL <kasarlabhargavi03@gmail.com>
2026-06-09 14:34:53 +0530

    about us page

--- src/app/about-us/about-us.ts
+++ src/app/about-us/about-us.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-about-us',
@@ -6,4 +6,38 @@ import { Component } from '@angular/core';
   templateUrl: './about-us.html',
   styleUrl: './about-us.css',
 })
-export class AboutUs {}
+export class AboutUs implements AfterViewInit {
+
+  @ViewChild('swiper')
+  swiper!: ElementRef;
+
+  ngAfterViewInit() {
+
+    const swiperEl = this.swiper.nativeElement;
+
+Object.assign(swiperEl, {
+  slidesPerView: 'auto',
+  centeredSlides: true,
+  loop: true,
+  loopedSlides: 3, // Tells Swiper to safely handle the 3-image cycle
+  speed: 4000,
+  allowTouchMove: false,
+  autoplay: {
+    delay: 0,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: false,
+  },
+  effect: 'coverflow',
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 20,
+    depth: 100,
+    modifier: 1,
+    scale: 0.85,
+  },
+  navigation: true,
+});
+swiperEl.initialize();
+
+  }
+}
